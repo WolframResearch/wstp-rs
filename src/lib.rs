caeaa0d1f6324b3fc7895823b85f060907a21010
Connor Gray <accounts@connorgray.com>
2020-04-07 15:41:01 -0500

    Add `publish = false` to Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,3 @@
-#![allow(
-    non_snake_case,
-    non_upper_case_globals,
-    non_camel_case_types,
-)]
+#![allow(non_snake_case, non_upper_case_globals, non_camel_case_types)]
 // The name of this file comes from `build.rs`.
 include!(concat!(env!("OUT_DIR"), "/WSTP_bindings.rs"));
