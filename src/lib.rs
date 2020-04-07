62b28ad94f81d646e68edbc5d6aa5356f6c8d70f
Connor Gray <accounts@connorgray.com>
2020-04-07 14:58:36 -0500

    Uncomment import of generated bindings in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-// #![allow(
-//     non_snake_case,
-//     non_upper_case_globals,
-//     non_camel_case_types,
-// )]
-// // The name of this file comes from `build.rs`.
-// include!(concat!(env!("OUT_DIR"), "/WSTP_bindings.rs"));
+#![allow(
+    non_snake_case,
+    non_upper_case_globals,
+    non_camel_case_types,
+)]
+// The name of this file comes from `build.rs`.
+include!(concat!(env!("OUT_DIR"), "/WSTP_bindings.rs"));
