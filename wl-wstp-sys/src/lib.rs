f16321d5a861c77b7d311ebc346f31548b7d4181
Connor Gray <accounts@connorgray.com>
2020-04-24 10:56:22 -0500

    Silence improper_ctypes warnings in generated bindings
    This is currently due to i128/f128 appearing in the bindings

--- wl-wstp-sys/src/lib.rs
+++ wl-wstp-sys/src/lib.rs
@@ -1,3 +1,3 @@
-#![allow(non_snake_case, non_upper_case_globals, non_camel_case_types)]
+#![allow(non_snake_case, non_upper_case_globals, non_camel_case_types, improper_ctypes)]
 // The name of this file comes from `build.rs`.
 include!(concat!(env!("OUT_DIR"), "/WSTP_bindings.rs"));
