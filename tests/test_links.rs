18f5f937936ae153951b712a54fd50302da9b794
Connor Gray <connorg@wolfram.com>
2021-05-17 13:26:10 -0500

    Add testing helper method to send data between two links
    Squashed:
    
      * Extend check_send_data_across_link() helper to test reading and writing from both ends of the link
    
        Previously, one end of the link would only "put" data, and one end would only "get" data.
        Now both ends do get and put operations.

--- tests/test_links.rs
+++ tests/test_links.rs
@@ -10,6 +10,42 @@ fn random_link_name() -> String {
         .collect()
 }
 
+// Helper method to check that data can successfully be sent from `link_a` to `link_b`.
+//
+// This tests reading and writing from both ends of the link.
+fn check_send_data_across_link(mut link_a: WstpLink, mut link_b: WstpLink) {
+    let thread_a = std::thread::spawn(move || {
+        link_a.activate().expect("failed to activate Listener side");
+
+        // Write an integer.
+        link_a.put_i64(5).unwrap();
+        link_a.flush().unwrap();
+
+        // Read a f64 written by the other side.
+        let got = link_a.get_f64().unwrap();
+        assert_eq!(got, 3.1415);
+
+        link_a
+    });
+
+    let thread_b = std::thread::spawn(move || {
+        link_b
+            .activate()
+            .expect("failed to activate Connector side");
+
+        let got = link_b.get_i64().unwrap();
+        assert_eq!(got, 5);
+
+        link_b.put_f64(3.1415).unwrap();
+        link_b.flush().unwrap();
+
+        link_b
+    });
+
+    let _link_a = thread_a.join().unwrap();
+    let _link_b = thread_b.join().unwrap();
+}
+
 //======================================
 // IntraProcess
 //======================================
@@ -21,41 +57,16 @@ fn test_intra_process_links() {
 
     let env = wstp::initialize().unwrap();
 
-    let mut listener = WstpLink::listen(&env, Protocol::IntraProcess, "").unwrap();
+    let listener = WstpLink::listen(&env, Protocol::IntraProcess, "").unwrap();
 
     // FIXME: IntraProcess-mode links ignore the `-linkname` device parameter and instead
     //        generate their own random string to use as a name. So we have to create the
     //        listener device first and then ask for it's name.
     let name = listener.link_name();
 
-    let listener_thread = std::thread::spawn(move || {
-        listener
-            .activate()
-            .expect("failed to activate Listener side");
+    let connector = WstpLink::connect(&env, Protocol::IntraProcess, &name).unwrap();
 
-        listener.put_i64(5).unwrap();
-        listener.flush().unwrap();
-
-        listener
-    });
-
-    let mut connector =
-        WstpLink::connect(&env, Protocol::IntraProcess, &name).unwrap();
-
-    let connector_thread = std::thread::spawn(move || {
-        connector
-            .activate()
-            .expect("failed to activate Connector side");
-
-
-        let got = connector.get_i64().unwrap();
-        assert_eq!(got, 5);
-
-        connector
-    });
-
-    let _listener = listener_thread.join().unwrap();
-    let _connector = connector_thread.join().unwrap();
+    check_send_data_across_link(listener, connector);
 }
 
 /// FIXME: IntraProcess-mode links ignore the `-linkname` device parameter and instead
@@ -78,33 +89,8 @@ fn test_bug_intra_process_device_ignored_linkname() {
 fn test_tcpip_links() {
     let env = wstp::initialize().unwrap();
 
-    let mut listener = WstpLink::listen(&env, Protocol::TCPIP, "8080").unwrap();
+    let listener = WstpLink::listen(&env, Protocol::TCPIP, "8080").unwrap();
+    let connector = WstpLink::connect(&env, Protocol::TCPIP, "8080").unwrap();
 
-    let listener_thread = std::thread::spawn(move || {
-        listener
-            .activate()
-            .expect("failed to activate Listener side");
-
-        listener.put_i64(5).unwrap();
-        listener.flush().unwrap();
-
-        listener
-    });
-
-    let mut connector =
-        WstpLink::connect(&env, Protocol::TCPIP, "8080").unwrap();
-
-    let connector_thread = std::thread::spawn(move || {
-        connector
-            .activate()
-            .expect("failed to activate Connector side");
-
-        let got = connector.get_i64().unwrap();
-        assert_eq!(got, 5);
-
-        connector
-    });
-
-    let _listener = listener_thread.join().unwrap();
-    let _connector = connector_thread.join().unwrap();
+    check_send_data_across_link(listener, connector);
 }
