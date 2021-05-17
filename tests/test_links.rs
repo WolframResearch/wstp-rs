9c21729ec927ccad6dacaccbcd89e6352da829d4
Connor Gray <connorg@wolfram.com>
2021-05-17 13:19:02 -0500

    Add basic test for TCPIP links

--- tests/test_links.rs
+++ tests/test_links.rs
@@ -69,3 +69,42 @@ fn test_bug_intra_process_device_ignored_linkname() {
     let listener = WstpLink::listen(&env, Protocol::IntraProcess, &name).unwrap();
     assert!(name != listener.link_name())
 }
+
+//======================================
+// TCPIP
+//======================================
+
+#[test]
+fn test_tcpip_links() {
+    let env = wstp::initialize().unwrap();
+
+    let mut listener = WstpLink::listen(&env, Protocol::TCPIP, "8080").unwrap();
+
+    let listener_thread = std::thread::spawn(move || {
+        listener
+            .activate()
+            .expect("failed to activate Listener side");
+
+        listener.put_i64(5).unwrap();
+        listener.flush().unwrap();
+
+        listener
+    });
+
+    let mut connector =
+        WstpLink::connect(&env, Protocol::TCPIP, "8080").unwrap();
+
+    let connector_thread = std::thread::spawn(move || {
+        connector
+            .activate()
+            .expect("failed to activate Connector side");
+
+        let got = connector.get_i64().unwrap();
+        assert_eq!(got, 5);
+
+        connector
+    });
+
+    let _listener = listener_thread.join().unwrap();
+    let _connector = connector_thread.join().unwrap();
+}
