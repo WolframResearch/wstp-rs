c31808926ffe3f48adb1090132cfc25913b9faf2
Connor Gray <connorg@wolfram.com>
2021-11-09 23:52:50 -0600

    format: save incidental changes to .rustfmt.toml

--- tests/test_link_server.rs
+++ tests/test_link_server.rs
@@ -53,9 +53,11 @@ fn test_link_server_using_accept() {
     //
 
     // Create a connection to the LinkServer, and exchange some data.
-    let mut link = WstpLink::connect_with_options(Protocol::TCPIP, &PORT.to_string(), &[
-        "MLUseUUIDTCPIPConnection",
-    ])
+    let mut link = WstpLink::connect_with_options(
+        Protocol::TCPIP,
+        &PORT.to_string(),
+        &["MLUseUUIDTCPIPConnection"],
+    )
     .unwrap();
 
     assert_eq!(link.get_i64(), Ok(0));
